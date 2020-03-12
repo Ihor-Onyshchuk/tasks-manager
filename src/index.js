12fc68dc8703709a869eb455bbb59d67e50ff1a0
Ihor_Onyshchuk <34838192+Ihor-Onyshchuk@users.noreply.github.com>
2020-03-12 20:14:33 +0200

    Add search
    * added search on todo  items
    
    * Add main part of app
    
    * added components and implemented visual interface
    
    * implemented logic behavior of the Todo app
    
    * refactored previous edits
    
    * refactored (TodoList & TodoItem) components
    
    * renamed 'TodoItem' component to 'TodoListItem'
    
    * Fixed issues

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import { render } from "react-dom";
 import App from "./components/App/App";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+
 render(<App />, document.getElementById("root"));
