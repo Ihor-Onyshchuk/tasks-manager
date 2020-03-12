dd120bfb4bba3ae6968effa9342272b3eb3d4162
Ihor-Onyshchuk <onyshchukihor11@gmail.com>
2020-03-12 13:15:40 +0200

    refactored (TodoList & TodoItem) components

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,23 +1,13 @@
 import React from "react";
-import TodoListItem from "../TodoListItem/TodoListItem";
 import "./TodoList.scss";
+import TodoItem from "../TodoItem/TodoItem";
 
-const TodoList = ({ todos, onDeleted, onToggleImportant, onToggleDone }) => {
-  const elements = todos.map(item => {
-    const { id, ...itemProps } = item;
-
-    return (
-      <li key={id} className="list-group-item">
-        <TodoListItem
-          {...itemProps}
-          onDeleted={() => onDeleted(id)}
-          onToggleImportant={() => onToggleImportant(id)}
-          onToggleDone={() => onToggleDone(id)}
-        />
-      </li>
-    );
-  });
-  return <ul className="list-group todo-list">{elements}</ul>;
-};
+const TodoList = ({ todos, ...otherProps }) => (
+  <ul className="list-group todo-list">
+    {todos.map(item => (
+      <TodoItem key={item.id} todoItem={item} {...otherProps} />
+    ))}
+  </ul>
+);
 
 export default TodoList;
