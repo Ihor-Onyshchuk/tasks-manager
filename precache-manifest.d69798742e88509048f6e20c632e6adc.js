75258a3d6aae772ac401e467cab88c861d789aef
Ihor-Onyshchuk <onyshchukihor11@gmail.com>
2020-03-30 10:54:07 +0300

    Updates

---
+++ precache-manifest.d69798742e88509048f6e20c632e6adc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94ea1bc79845de14f448417abb019d90",
+    "url": "/Todo-App/index.html"
+  },
+  {
+    "revision": "560f2dafcc4b07369ae2",
+    "url": "/Todo-App/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "81308c7c1d4662dddb57",
+    "url": "/Todo-App/static/css/main.96cb1c57.chunk.css"
+  },
+  {
+    "revision": "560f2dafcc4b07369ae2",
+    "url": "/Todo-App/static/js/2.450996eb.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Todo-App/static/js/2.450996eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81308c7c1d4662dddb57",
+    "url": "/Todo-App/static/js/main.c665bcb0.chunk.js"
+  },
+  {
+    "revision": "1ef1aedd45397a987598",
+    "url": "/Todo-App/static/js/runtime-main.9c1ec44f.js"
+  }
+]);
